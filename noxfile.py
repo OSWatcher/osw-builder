69c0323015c6ff428d15d99db6bdeabaa0132c35
Mathieu Tarral <mathieu.tarral@protonmail.com>
2020-10-04 14:04:57 +0200

    nox: add run session

--- noxfile.py
+++ noxfile.py
@@ -21,3 +21,10 @@ def type(session):
 def fmt(session):
     session.install("black")
     session.run("black", "--line-length", "120", ".")
+
+
+@nox.session
+def run(session):
+    args = session.posargs
+    session.install("-r", "requirements.txt")
+    session.run("python", "builder.py", *args)
