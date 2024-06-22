348c864732bc0f576de9859819c2617397f7ea1a
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-06-22 17:56:04 +0200

    vagrant: add get_qcow_path

---
+++ osw_builder/vagrant/lib_virt.py
@@ -0,0 +1,23 @@
+from contextlib import contextmanager
+from pathlib import Path
+from typing import Generator
+
+import libvirt
+
+from .domxml import DomXML
+
+
+@contextmanager
+def virt_con_ctxt(uri) -> Generator[libvirt.virConnect, None, None]:
+    con = libvirt.open(uri)
+    try:
+        yield con
+    finally:
+        con.close()
+
+
+def get_qcow_path(domaine_name: str, uri: str = "qemu:///system") -> Path:
+    with virt_con_ctxt(uri) as con:
+        domain = con.lookupByName(domaine_name)
+        dom_xml = DomXML(domain.XMLDesc())
+        return Path(dom_xml.disk)
