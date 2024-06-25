ec6dd10587f96982b8cf720e93c6b99fa3f817f3
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-06-25 03:00:07 +0200

    main: apply updates and recapture

--- osw_builder/__main__.py
+++ osw_builder/__main__.py
@@ -16,13 +16,14 @@ Options:
 
 
 import logging
-from contextlib import ExitStack
+from contextlib import ExitStack, suppress
 
 from docopt import docopt
+from winupdate.winupdate import UpdateNotInstalledError, WinUpdate
 
 from osw_builder import vagrant
 from osw_builder.build import build_image
-from osw_builder.capture import capture_neogit
+from osw_builder.capture import capture_neogit, create_branch
 from osw_builder.settings import settings
 
 BUILD_SNAPSHOT_NAME = "build"
@@ -101,11 +102,57 @@ def main(args):
                         vagrant.define(vagrant_dir)
                         vagrant.snapshot_save(vagrant_dir, BUILD_SNAPSHOT_NAME)
 
-                for snapshot in vagrant.snapshot_list(vagrant_dir):
-                    vagrant.snapshot_restore(vagrant_dir, snapshot)
-                    qcow_path = vagrant.get_qcow_path(box_name, uri=LIBVIRT_URI)
-                    logging.debug("Qcow path: %s", qcow_path)
-                    capture_neogit(qcow_path, box_name)
+                # get the qcow path
+                qcow_path = vagrant.get_qcow_path(box_name, uri=LIBVIRT_URI)
+                logging.debug("Qcow path: %s", qcow_path)
+
+                # restore build snapshot
+                vagrant.snapshot_restore(vagrant_dir, BUILD_SNAPSHOT_NAME)
+                build_commit = capture_neogit(qcow_path, box_name, unique=True)
+
+                # create branch
+                branch_name = box_name
+                with suppress(ValueError):
+                    create_branch(
+                        branch_name,
+                        build_commit,
+                    )
+
+                # TODO idle state
+
+                # loop through the snapshot list, and assert that the first one is the build snapshot
+                snap_list = vagrant.snapshot_list(vagrant_dir, qcow_path)
+                assert snap_list[0].Tag == BUILD_SNAPSHOT_NAME
+
+                # iterate after 'build' snapshot
+                for snap in snap_list[1:]:
+                    vagrant.snapshot_restore(vagrant_dir, snap.Tag)
+                    capture_neogit(qcow_path, snap.Tag, branch_name, unique=True)
+
+                # take last snapshot
+                previous_snap = snap_list[-1].Tag
+                # apply latest winupdates
+                with vagrant.up_down_ctxt(vagrant_dir):
+                    logging.info("Searching for Windows Updates")
+                    winrm_config = vagrant.winrm_config(vagrant_dir)
+                    win_update = WinUpdate(winrm_config.HostName, debug_lvl=0)
+                    for index, update in enumerate(win_update.search()):
+                        kb_name = f"KB-{update.kb[0]}"
+                        logging.info("[%s][%s] %s", index + 1, kb_name, update.title)
+                        try:
+                            with vagrant.up_down_ctxt(vagrant_dir):
+                                win_update.apply_update(update.id, update.kb[0])
+                        except UpdateNotInstalledError:
+                            logging.warning("Update not installed")
+                            # restore previous snapshot
+                            vagrant.snapshot_restore(vagrant_dir, previous_snap)
+                        else:
+                            # SUCCESS !
+                            # take snapshot
+                            vagrant.snapshot_save(vagrant_dir, kb_name)
+                            # update previous
+                            previous_snap = kb_name
+                            capture_neogit(qcow_path, kb_name, branch_name, unique=True)
 
 
 def entrypoint():
