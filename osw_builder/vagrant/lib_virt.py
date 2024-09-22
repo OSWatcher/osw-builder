b57a5bfd011b656ede4ac35ed9a82bf2c65ec662
Mathieu Tarral <mathieu.tarral@protonmail.com>
2024-09-22 15:39:50 +0200

    builder: ensure refresh libvirt pool

--- osw_builder/vagrant/lib_virt.py
+++ osw_builder/vagrant/lib_virt.py
@@ -21,3 +21,9 @@ def get_qcow_path(domaine_name: str, uri: str = "qemu:///system") -> Path:
         domain = con.lookupByName(domaine_name)
         dom_xml = DomXML(domain.XMLDesc())
         return Path(dom_xml.disk)
+
+
+def pool_refresh(uri: str = "qemu:///system", pool_name: str = "default"):
+    with virt_con_ctxt(uri) as con:
+        pool = con.storagePoolLookupByName(pool_name)
+        pool.refresh()
